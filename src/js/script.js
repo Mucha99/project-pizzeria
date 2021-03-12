55e71f9e1b4b68256586004618df4b3d186f8831
Bartek Mucha <bartekmucha43@gmail.com>
2021-03-12 13:51:11 +0100

    Adding a product to the cart

--- src/js/script.js
+++ src/js/script.js
@@ -105,6 +105,7 @@
       thisProduct.initOrderForm();
       thisProduct.initAmountWidget();
       thisProduct.processOrder();
+      thisProduct.prepareCartProductParams();
 
       // console.log(`new product:`, thisProduct);
     }
@@ -186,6 +187,7 @@
       thisProduct.cartButton.addEventListener(`click`, function (event) {
         event.preventDefault();
         thisProduct.processOrder();
+        thisProduct.addToCart();
       });
 
       // console.log(`initOrderForm:`, this.initOrderForm);
@@ -242,7 +244,9 @@
       }
       /* multiply price by amount*/
       price *= thisProduct.amountWidget.value;
-      //activeProduct.classList.remove(`classNames.menuProduct.imageVisible`);
+
+      // price for cart
+      thisProduct.priceSingle = price;
 
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
@@ -256,6 +260,56 @@
       thisProduct.amountWidgetElem.addEventListener(`updated`, function () {
         thisProduct.processOrder();
       });
+    }
+
+    addToCart() {
+      const thisProduct = this;
+
+      app.cart.add(thisProduct.prepareCartProduct());
+    }
+
+    prepareCartProduct() {
+      const thisProduct = this;
+
+      const productSummary = {
+        id: thisProduct.id,
+        name: thisProduct.data.name,
+        amount: thisProduct.amountWidget.value,
+        priceSingle: thisProduct.priceSingle,
+        price: thisProduct.priceSingle * thisProduct.amountWidget.value,
+        params: thisProduct.prepareCartProductParams(),
+      };
+      return productSummary;
+    }
+
+    prepareCartProductParams() {
+      const thisProduct = this;
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      const params = {};
+
+      // for every category (param)...
+      for (let paramId in thisProduct.data.params) {
+        const param = thisProduct.data.params[paramId];
+
+        // create category param in params const eg. params = { ingredients: { name: 'Ingredients', options: {}}}
+        params[paramId] = {
+          name: param.label,
+          options: {},
+        };
+
+        // for every option in this category
+        for (let optionId in param.options) {
+          const option = param.options[optionId];
+          const optionSelected =
+            formData[paramId] && formData[paramId].includes(optionId);
+
+          if (optionSelected) {
+            params[paramId].options[optionId] = option.label;
+          }
+        }
+      }
+      return params;
     }
   }
 
@@ -353,6 +407,9 @@
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(
         select.cart.toggleTrigger
       );
+      thisCart.dom.productList = thisCart.dom.wrapper.querySelector(
+        select.cart.productList
+      );
     }
 
     initActions() {
@@ -361,6 +418,16 @@
       thisCart.dom.toggleTrigger.addEventListener(`click`, function () {
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
       });
+    }
+
+    add(menuProduct) {
+      const thisCart = this;
+
+      const generatedHTML = templates.menuProduct(menuProduct.data);
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      thisCart.dom.productList.appendChild(generatedDOM);
+
+      console.log(`adding product`, menuProduct);
     }
   }
 
