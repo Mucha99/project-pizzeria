73a000bea6347f1ad27ef9a10ea99f3f12b47fa3
Bartek Mucha <bartekmucha43@gmail.com>
2021-03-18 19:54:42 +0100

    Remove the product

--- src/js/script.js
+++ src/js/script.js
@@ -421,6 +421,10 @@
       thisCart.dom.productList.addEventListener(`updated`, function () {
         thisCart.update();
       });
+
+      thisCart.dom.productList.addEventListener(`remove`, function (event) {
+        thisCart.remove(event.detail.cartProduct);
+      });
     }
 
     add(menuProduct) {
@@ -466,6 +470,17 @@
       console.log(`subtotalPrice:`, thisCart.subtotalPrice);
       console.log(`totalPrice:`, thisCart.totalPrice);
     }
+
+    remove(CartProduct) {
+      const thisCart = this;
+
+      CartProduct.dom.wrapper.remove();
+
+      const indexOfProduct = thisCart.products.indexOf(CartProduct);
+      thisCart.products.splice(indexOfProduct, 1);
+
+      thisCart.update();
+    }
   }
 
   class CartProduct {
@@ -481,6 +496,7 @@
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
     }
 
     getElements(element) {
@@ -515,6 +531,32 @@
         thisCartProduct.price =
           thisCartProduct.amount * thisCartProduct.priceSingle;
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+      });
+    }
+
+    remove() {
+      const thisCartProduct = this;
+
+      const event = new CustomEvent(`remove`, {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
+
+    initActions() {
+      const thisCartProduct = this;
+
+      thisCartProduct.dom.edit.addEventListener(`click`, function (event) {
+        event.preventDefault();
+      });
+
+      thisCartProduct.dom.remove.addEventListener(`click`, function (event) {
+        event.preventDefault();
+        thisCartProduct.remove();
       });
     }
   }
