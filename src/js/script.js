114709c88ec96bbbc0cfbb5dfa8d130f9f7dccf5
Bartek Mucha <bartekmucha43@gmail.com>
2021-03-19 22:46:23 +0100

    Product list download - API

--- src/js/script.js
+++ src/js/script.js
@@ -77,6 +77,11 @@
       defaultDeliveryFee: 20,
     },
     // CODE ADDED END
+    db: {
+      url: `//localhost:3131`,
+      product: `product`,
+      order: `order`,
+    },
   };
 
   const templates = {
@@ -566,14 +571,33 @@
       const thisApp = this;
 
       for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(
+          thisApp.data.products[productData].id,
+          thisApp.data.products[productData]
+        );
       }
     },
 
     initData: function () {
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      thisApp.data = {};
+
+      const url = settings.db.url + `/` + settings.db.product;
+
+      fetch(url)
+        .then(function (rawResponse) {
+          return rawResponse.json();
+        })
+        .then(function (parsedResponse) {
+          console.log(`persedResponse`, parsedResponse);
+          /* save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+          /* execute initMenu method */
+          thisApp.initMenu();
+        });
+
+      console.log(`thisApp.data`, JSON.stringify(thisApp.data));
     },
 
     initCart: function () {
@@ -587,7 +611,6 @@
       const thisApp = this;
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
   };
