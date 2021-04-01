cf61fde15566759598afac333b3958441c88cfd8
Bartek Mucha <bartekmucha43@gmail.com>
2021-04-01 18:31:58 +0200

    error generated class-home-html

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import { settings, select, classNames } from "./settings.js";
 import Product from "./components/Product.js";
 import Cart from "./components/Cart.js";
 import Booking from "./components/Booking.js";
+import Home from "./components/Home.js";
 
 const app = {
   initPages: function () {
@@ -107,6 +108,13 @@ const app = {
     thisApp.booking = new Booking(bookingWidget);
   },
 
+  initHome: function () {
+    const thisApp = this;
+
+    const homeElement = document.querySelector(select.containerOf.home);
+    thisApp.home = new Home(homeElement);
+  },
+
   init: function () {
     const thisApp = this;
 
@@ -115,6 +123,7 @@ const app = {
     thisApp.initData();
     thisApp.initCart();
     thisApp.initBooking();
+    thisApp.initHome();
   },
 };
 
---
+++ src/js/components/Home.js
@@ -0,0 +1,22 @@
+/* eslint-disable quotes */
+import { templates } from "../settings.js";
+
+class Home {
+  constructor(element) {
+    const thisHome = this;
+
+    thisHome.render(element);
+  }
+
+  render(wrapper) {
+    const thisHome = this;
+
+    const generatedHTML = templates.homeWidget();
+
+    thisHome.dom = {};
+    thisHome.dom.wrapper = wrapper;
+    thisHome.dom.wrapper.innerHTML = generatedHTML;
+  }
+}
+
+export default Home;
